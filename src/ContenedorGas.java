dd450616d46b3b85c0f17a0c2ec8b2fc315a7fc3
Adri4Code <afdezramos@gmail.com>
2017-03-15 16:26:00 +0100

    PCD

--- src/ContenedorGas.java
+++ src/ContenedorGas.java
@@ -2,4 +2,8 @@
  * Created by adri on 15/03/17.
  */
 public class ContenedorGas {
+    public void cogerGas(){
+
+    }
+
 }
--- src/ZonaCarga.java
+++ src/ZonaCarga.java
@@ -1,17 +1,16 @@
 /**
  * Created by adri on 8/03/17.
- */ public class ZonaCarga {
+ *
+ */
+public class ZonaCarga {
     private int controlCarga;
     private ContenedorAgua aguita;
     private ContenedorGas gasolina;
-    private int capacidadAgua;
-    private int capacidadGasolina;
     private ContenedorGas[] contenedorGasolina= new ContenedorGas[5];
     public ZonaCarga(){
-        capacidadAgua=3000;
-        capacidadGasolina=1000;
+
         for (int i = 0; i <contenedorGasolina.length ; i++) {
-            //this.contenedorGasolina[i]=capacidadGasolina;
+            this.contenedorGasolina[i]=new ContenedorGas();
         }
 
     }
@@ -20,49 +19,13 @@
         if(barcos.getPetrolero()==true ){
             controlCarga++;
             if(controlCarga==5){
-                cargaPrivadaGas(barcos);
-                cargaPublicaAgua(barcos);
+                //cargarAgua
+                //cargarGas
+
             }
 
         }
 
     }
-    public void restablecerControl(){
-        this.controlCarga=0;
-    }
-     public void cargaPrivadaGas(Barcos barcos){
-        int cargaAux=250;
-        if(controlCarga==5){
 
-        }
-     }
-     public void cargaPublicaAgua(Barcos barcos ){
-         int cargaAux=500;
-        if(controlCarga==5){
-
-        }
-     }
-     public boolean gasesVacios(){
-         boolean vacio= false;
-         int aux=0;
-         for (int i = 0; i <contenedorGasolina.length ; i++) {
-
-         }
-             /*if(contenedorGasolina[i]==0){
-                 aux++;
-             }
-         }
-         if(aux==5){
-             vacio=true;
-         }*/
-         return vacio;
-     }
-     public void rellenarContenedoresGas(){
-         if(gasesVacios()){
-             for (int i = 0; i <contenedorGasolina.length ; i++) {
-               //  this.contenedorGasolina[i]=capacidadGasolina;
-             }
-
-         }
-     }
 }
